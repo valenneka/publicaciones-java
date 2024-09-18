78af5b3cba9b38fcbfbbf744a0dacb6ec43cc37c
SxxnDev <strxpensupp0rt@gmail.com>
2024-09-17 21:16:11 -0300

    Programa terminado

---
+++ src/libros.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author valen
+ */
+public class libros extends publicaciones {
+    private String tema;
+    private String autor;
+
+    public String getTema() {
+        return tema;
+    }
+
+    public void setTema(String tema) {
+        this.tema = tema;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAnioCreacion() {
+        return anioCreacion;
+    }
+
+    public void setAnioCreacion(String anioCreacion) {
+        this.anioCreacion = anioCreacion;
+    }
+
+    public boolean isPrestada() {
+        return prestada;
+    }
+
+    public void setPrestada(boolean prestada) {
+        this.prestada = prestada;
+    }
+    
+    
+}
---
+++ src/revistas.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author valen
+ */
+public class revistas extends publicaciones {
+    private String tipo;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAnioCreacion() {
+        return anioCreacion;
+    }
+
+    public void setAnioCreacion(String anioCreacion) {
+        this.anioCreacion = anioCreacion;
+    }
+
+    public boolean isPrestada() {
+        return prestada;
+    }
+
+    public void setPrestada(boolean prestada) {
+        this.prestada = prestada;
+    }
+    
+}
